a4e45b71ed87a3745da9b14d3c030c95db2fb7b7
kundlam <54889718+kundlam@users.noreply.github.com>
2019-09-08 23:11:28 +0200

    Add files via upload

---
+++ Hazifeladat/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazifeladat
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int a = 0;
+            bool validUserInput = false;
+
+            while (validUserInput == false)
+            {
+                try
+                {
+                    Console.Write("Add meg az A oldalt:  ");
+                    a = int.Parse(Console.ReadLine());
+                }
+                catch (Exception) { } 
+
+                if (a >= 1)
+                {
+                    validUserInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Nem számot írtál be");
+                }
+            }
+            int b = 0;
+            validUserInput = false;
+
+            while (validUserInput == false)
+            {
+                try
+                {
+                    Console.Write("Add meg a B oldalt:  ");
+                    b = int.Parse(Console.ReadLine());
+                }
+                catch (Exception) { }
+                if (b >= 1)
+                {
+                    validUserInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Nem számot írtál be");
+                }
+
+            }
+            double c = a * b;
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Public\TestFolder\WriteLines.txt"))
+            file.WriteLine(c)
+            Console.WriteLine("A két szám szorzata: " + a*b);
+
+            Console.ReadKey();
+        }
+    }
+}
